7f640869cbddcfefe2d5380f3544a1ef9747c09d
Hyuse <165687332+hyus3@users.noreply.github.com>
2025-04-21 15:03:28 +0800

    test

---
+++ test/Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        HScrollBar1 = New HScrollBar()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' HScrollBar1
+        ' 
+        HScrollBar1.Location = New Point(193, 400)
+        HScrollBar1.Name = "HScrollBar1"
+        HScrollBar1.Size = New Size(100, 26)
+        HScrollBar1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(200, 155)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 1
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(HScrollBar1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents HScrollBar1 As HScrollBar
+    Friend WithEvents Button1 As Button
+
+End Class
